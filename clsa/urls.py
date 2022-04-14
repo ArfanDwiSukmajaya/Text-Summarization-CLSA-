de59273515358def7a5ed06c3f5cb8fdcb44964d
ArfanDwiSukmajaya <jayaarfan92@gmail.com>
2022-04-14 13:05:14 +0700

    update untuk hosting

--- clsa/urls.py
+++ clsa/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+
+from django.conf.urls import url
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('proses', views.proses)
+    path('proses', views.proses),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
