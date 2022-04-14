c90d138bb8b2a4a5c2f24c1e6eca4ecf480c3e9b
ArfanDwiSukmajaya <jayaarfan92@gmail.com>
2022-04-14 13:15:27 +0700

    setting ALLOWED_HOST

--- clsa/settings.py
+++ clsa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+)(7kid7!fa!%n^cr&bhjea31^^202qyh!k+&p3w@6@b&cbs9^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'ats-clsa.herokuapp.com']
 
 
 # Application definition
