6993fcc41142e3187fce3fec94dcc8a00279f2fc
ArfanDwiSukmajaya <jayaarfan92@gmail.com>
2022-04-18 23:03:05 +0700

    update

--- clsa/urls.py
+++ clsa/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+# from django.conf import settings
+# from django.conf.urls.static import static
 from . import views
 
-from django.conf.urls import url
-from django.views.static import serve
+# from django.conf.urls import url
+# from django.views.static import serve
 
 
 urlpatterns = [
